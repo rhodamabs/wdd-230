204ed09b4af50489f81a265ff734fcfda0fff63b
rhodamabs <mab21004@byui.edu>
2022-07-24 00:51:08 +0200

    Commit

--- temple-site/script/forecast.js
+++ temple-site/script/forecast.js
@@ -1,16 +1,18 @@
+const url ='..//api.openweathermap.org/data/2.5/forecast?q=bethesda&appid=a87edcd23002a231974ad3dd082effc8'
 
-
-fetch('https://api.openweathermap.org/data/2.5/forecast?q=bethesda&appid=a87edcd23002a231974ad3dd082effc8')
-.then(response => response.json())
-.then(data =>{
-    for(1=0; 1<5;i++){
+fetch(url)
+.then(function(response) {response.json()})
+.then(function (jsonObject){
+    const data = jsonObject;
+    console.log(data);
+    for(i=0; i<5;i++){
         document.getElementById('day' +(i+1)+ "Min").innerHTML = "Min:" + Number(data.list.main[i].main.temp_min - 288.53).toFixed(1)+ "°";
     }
-    for(1=0; 1<5;i++){
+    for(i=0; i<5;i++){
         document.getElementById('day' +(i+1)+ "Max").innerHTML = "Max:" + Number(data.list.main[i].main.temp_max - 288.53).toFixed(1)+ "°";
     }
-    for(1=0; 1<5;i++){
-        document.getElementById('img' +(i+1)).src ="https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon+".png";
+    for(i=0; i<5;i++){
+        document.getElementById('img' +(i+1)).src ="//openweathermap.org/img/wn/" + data.list[i].weather[0].icon+".png";
     }
 })
 
@@ -26,6 +28,6 @@ function checkDay(day){
     }
 }
 
-for(1=0; 1<5;i++){
+for(i=0; i<5;i++){
     document.getElementById('day' +(i+1)).innerHTML = weekday[checkDay(i)];
 }
