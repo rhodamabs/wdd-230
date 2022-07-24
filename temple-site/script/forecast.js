c9cde9f297fce3673eae9df667dd149c79560d3f
rhodamabs <mab21004@byui.edu>
2022-07-24 02:37:28 +0200

    Commit

--- temple-site/script/forecast.js
+++ temple-site/script/forecast.js
@@ -1,33 +1,50 @@
-const url ='..//api.openweathermap.org/data/2.5/forecast?q=bethesda&appid=a87edcd23002a231974ad3dd082effc8'
 
-fetch(url)
-.then(function(response) {response.json()})
-.then(function (jsonObject){
-    const data = jsonObject;
-    console.log(data);
-    for(i=0; i<5;i++){
-        document.getElementById('day' +(i+1)+ "Min").innerHTML = "Min:" + Number(data.list.main[i].main.temp_min - 288.53).toFixed(1)+ "°";
-    }
-    for(i=0; i<5;i++){
-        document.getElementById('day' +(i+1)+ "Max").innerHTML = "Max:" + Number(data.list.main[i].main.temp_max - 288.53).toFixed(1)+ "°";
-    }
-    for(i=0; i<5;i++){
-        document.getElementById('img' +(i+1)).src ="//openweathermap.org/img/wn/" + data.list[i].weather[0].icon+".png";
-    }
-})
 
-const d = new Date;
-const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const forecastURL ="//api.openweathermap.org/data/2.5/onecall?lat=17.8216&lon=31.0492&exclude=minutely,hourly&units=imperial&appid=a87edcd23002a231974ad3dd082effc8";
+fetch(forecastURL)
+  .then((response) => response.json())
+  .then((jsonObject) => {
+    const dailyForecast = jsonObject.daily.slice(0, 4);
+    console.log(jsonObject);
 
-function checkDay(day){
-    if(day +d.getDay() > 6){
-        return day +d.getDay()-7;
-    }
-    else{
-        return day +d.getDay();
-    }
-}
+    const d = new Date();
 
-for(i=0; i<5;i++){
-    document.getElementById('day' +(i+1)).innerHTML = weekday[checkDay(i)];
-}
+    const todayDayNumber = d.getDay();
+
+    const weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    let forecastDayNumber = todayDayNumber;
+
+    dailyForecast.forEach((weather) => {
+      forecastDayNumber += 1;
+      if (forecastDayNumber === 7) {
+        forecastDayNumber = 0;
+      }
+      const forecastItem = document.createElement("div");
+      forecastItem.classList = "forecast-item";
+      const dayName = document.createElement("h3");
+      dayName.textContent = weekday[forecastDayNumber];
+
+      const iconPath =
+        "//openweathermap.org/img/wn/" + weather.weather[0].icon + "@2x.png";
+      const icon = document.createElement("img");
+      icon.src = iconPath;
+      icon.alt = weather.weather[0].description;
+
+      const temp = document.createElement("p");
+      temp.textContent = weather.temp.day + "\xB0F";
+
+      forecastItem.appendChild(dayName);
+      forecastItem.appendChild(icon);
+      forecastItem.appendChild(temp);
+
+      document.querySelector(".fore-container").appendChild(forecastItem);
+    });
+  });
